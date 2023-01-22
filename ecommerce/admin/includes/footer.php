b4731d536d4252ad994d87d9777c1aba15ae6c37
KrebsZ <krebs653@gmail.com>
2023-01-22 16:56:07 +0700

    commit

---
+++ ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>ติดต่อธีรภัทร์ บริบูรณ์</b>
+    </div>
+     <strong>Copyright &copy; 2022 <a href="https://www.facebook.com/BermzISware">KKU อุปกรณ์ก่อสร้าง</a></strong>
+</footer>
---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>ติดต่อเรา 012-345-6789</b>
+      </div>
+      <strong>Copyright &copy; 2022 <a href="https://kku.ac.th/">KKU อุปกรณ์ก่อสร้างอันดับ 1 ในจังหวัด ขอนเเก่น</a></strong>
+    </div>
+</footer>
